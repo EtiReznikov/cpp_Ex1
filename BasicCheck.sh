b94fa7974daf7b68fc1db9bd23030ab7bf7d750f
EtiReznikov <44700034+EtiReznikov@users.noreply.github.com>
2019-03-11 20:29:35 +0200

    Add files via upload

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -7,7 +7,6 @@ folderName=$1
 executable=$2
 currentfolder=$(pwd)
 
-Otherarguments=""${@:2}""
 
 ans=7
 Compliation="FAIL"
@@ -19,39 +18,38 @@ make
 secssesfullmake=$?
 
 if [[ $secssesfullMake -eq 0 ]]; then
+valgrind --leak-check=full --error-exitcode=1  ./$executable "${@:2}" > /dev/null 2&>1
+  valgridgout=$?
+  valgrind --tool=helgrind --error-exitcode=1 ./$executable "${@:2}"> /dev/null 2&>1
+  helgrindout=$?
 
-valgrind --leak-check=full --error-exitcode=3  ./$executable $Otherarguments > /dev/null 2&>1
-valgridgout=$?
-valgrind --tool=helgrind --error-exitcode=3 ./$executable $Otherarguments > /dev/null 2&>1
-helgrindout=$?
+   if [[ $valgridgout -ne 1 && $helgrindout -ne 1 ]]; then
+    Compliation="PASS"
+    Memory_leaks="PASS"
+    Thread_race="PASS"
+    ans=0
+   fi
 
-if [[ $valgridgout -ne 3 && $helgrindout -ne 3 ]]; then
-Compliation="PASS"
-Memory_leaks="PASS"
-Thread_race="PASS"
-ans=0
-fi
+   if [[ $valgridgout -eq 1 && $helgrindout -ne 1 ]]; then
+    Compliation="PASS"
+    Memory_leaks="FAIL"
+    Thread_race="PASS"
+    ans=2
+   fi
 
-if [[ $valgridgout -eq 3 && $helgrindout -ne 3 ]]; then
-Compliation="PASS"
-Memory_leaks="FAIL"
-Thread_race="PASS"
-ans=2
-fi
+   if [[ $valgridgout -ne 1 && $helgrindout -eq 1 ]]; then
+    Compliation="PASS"
+    Memory_leaks="PASS"
+    Thread_race="FAIL"
+    ans=1
+   fi
 
-if [[ $valgridgout -ne 3 && $helgrindout -eq 3 ]]; then
-Compliation="PASS"
-Memory_leaks="PASS"
-Thread_race="FAIL"
-ans=1
-fi
-
-if [[ $valgridgout -eq 3 && $helgrindout -eq 3 ]]; then
-Compliation="PASS"
-Memory_leaks="FAIL"
-Thread_race="FAIL"
-ans=3
-fi
+   if [[ $valgridgout -eq 1 && $helgrindout -eq 1 ]]; then
+    Compliation="PASS"
+    Memory_leaks="FAIL"
+    Thread_race="FAIL"
+    ans=3
+   fi
 
 fi
 
