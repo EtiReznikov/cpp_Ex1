d1333cc86961c243200497f71d8852d316fae752
EtiReznikov <44700034+EtiReznikov@users.noreply.github.com>
2019-03-11 19:46:14 +0200

    Add files via upload

--- Basiccheck.sh
+++ Basiccheck.sh
@@ -15,49 +15,39 @@ secssesfullmake=$?
 if [[ $secssesfullMake -ne 0 ]]; then
 echo "Compilation fail"
 cd $currentfolder
-$ans=7
-echo $ans
+ans=7
 exit $ans
 fi
 
-valgrind --leak-check=full --error-exitcode=3  ./$executable 
+valgrind --leak-check=full --error-exitcode=3  ./$executable > /dev/null 2&>1
 valgridgout=$?
-valgrind --tool=helgrind --error-exitcode=3 ./$executable 
+valgrind --tool=helgrind --error-exitcode=3 ./$executable > /dev/null 2&>1
 helgrindout=$?
 
 echo "Compilation| Memory leaks| thread race" 
 
 if [[ $valgridgout -ne 3 && $helgrindout -ne 3 ]]; then
 echo "    PASS   |     PASS    |     PASS   " 
-cd $currentfolder
-$ans=0
-echo $ans
-exit 0
+ans=0
 fi
 
 if [[ $valgridgout -eq 3 && $helgrindout -ne 3 ]]; then
 echo "    PASS   |     FAIL    |     PASS   " 
-cd $currentfolder
-$ans=1
-echo $ans
-exit 1
+ans=1
 fi
 
 if [[ $valgridgout -ne 3 && $helgrindout -eq 3 ]]; then
 echo "    PASS   |     PASS    |     FAIL   " 
-cd $currentfolder
-$ans=2
-echo $ans
-exit 2
+ans=2
 fi
 
 if [[ $valgridgout -eq 3 && $helgrindout -eq 3 ]]; then
 echo "    PASS   |     FAIL    |     FAIL   " 
-cd $currentfolder
-$ans=3
-echo $ans
-exit 3
+ans=3
 fi
+
+cd $currentfolder
+exit $ans
 
 
 
