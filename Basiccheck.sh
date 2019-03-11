82f1cd054c2058d668c4d0237ad7e2c74bcd3a96
EtiReznikov <44700034+EtiReznikov@users.noreply.github.com>
2019-03-11 12:58:24 +0200

    Add files via upload

--- Basiccheck.sh
+++ Basiccheck.sh
@@ -1,48 +1,51 @@
-# Estar reznikov 315674028 
-# Yaara goldenberg 308529296
+
 
 #!/bin/bash
+
+#Ester Reznikov 315674028
+#Yaara Goldenberg 308529296
 folderName=$1
 executable=$2
-
-if [ ! -f Makefile ]; then
-echo "Makefile does not exist!"
-exit 7
-fi
+currentfolder=$(pwd)
 
 cd $folderName
 make
 secssesfullmake=$?
 
-if [ $secssesfullMake -eq 0 ]; then
+if [[ $secssesfullMake -ne 0 ]]; then
 echo "Compilation fail"
+cd $currentfolder
 exit 7
 fi
 
-valgrind --leak-check=full --error-exitcode=3 ./$executable
+valgrind --leak-check=full --error-exitcode=3  ./$executable @$2 >/dev/null 2>/dev/null
 valgridgout=$?
-valgrind --tool=helgrind --error-exitcode=3 ./$executable
+valgrind --tool=helgrind --error-exitcode=3 ./$executable @$2 >/dev/null 2>/dev/null
 helgrindout=$?
 
 echo "Compilation| Memory leaks| thread race" 
 
 if [[ $valgridgout -ne 3 && $helgrindout -ne 3 ]]; then
 echo "    PASS   |     PASS    |     PASS   " 
+cd $currentfolder
 exit 0
 fi
 
 if [[ $valgridgout -eq 3 && $helgrindout -ne 3 ]]; then
 echo "    PASS   |     FAIL    |     PASS   " 
+cd $currentfolder
 exit 1
 fi
 
 if [[ $valgridgout -ne 3 && $helgrindout -eq 3 ]]; then
 echo "    PASS   |     PASS    |     FAIL   " 
+cd $currentfolder
 exit 2
 fi
 
 if [[ $valgridgout -eq 3 && $helgrindout -eq 3 ]]; then
 echo "    PASS   |     FAIL    |     FAIL   " 
+cd $currentfolder
 exit 3
 fi
 
