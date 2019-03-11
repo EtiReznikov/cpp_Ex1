7277841269e48e5a59bf02e98bd7c95dded3022e
EtiReznikov <44700034+EtiReznikov@users.noreply.github.com>
2019-03-11 20:41:39 +0200

    Add files via upload

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -9,6 +9,8 @@ currentfolder=$(pwd)
 
 
 ans=7
+echo $ans
+
 Compliation="FAIL"
 Memory_leaks="FAIL"
 Thread_race="FAIL"
@@ -20,31 +22,31 @@ secssesfullmake=$?
 if [[ $secssesfullMake -eq 0 ]]; then
 valgrind --leak-check=full --error-exitcode=1  ./$executable "${@:2}" > /dev/null 2&>1
   valgridgout=$?
-  valgrind --tool=helgrind --error-exitcode=1 ./$executable "${@:2}"> /dev/null 2&>1
+valgrind --tool=helgrind --error-exitcode=1 ./$executable "${@:2}"> /dev/null 2&>1
   helgrindout=$?
 
-   if [[ $valgridgout -ne 1 && $helgrindout -ne 1 ]]; then
+   if [[ $valgridgout -eq 1 && $helgrindout -eq 1 ]]; then
     Compliation="PASS"
     Memory_leaks="PASS"
     Thread_race="PASS"
     ans=0
    fi
 
-   if [[ $valgridgout -eq 1 && $helgrindout -ne 1 ]]; then
+   if [[ $valgridgout -ne 1 && $helgrindout -eq 1 ]]; then
     Compliation="PASS"
     Memory_leaks="FAIL"
     Thread_race="PASS"
     ans=2
    fi
 
-   if [[ $valgridgout -ne 1 && $helgrindout -eq 1 ]]; then
+   if [[ $valgridgout -eq 1 && $helgrindout -ne 1 ]]; then
     Compliation="PASS"
     Memory_leaks="PASS"
     Thread_race="FAIL"
     ans=1
    fi
 
-   if [[ $valgridgout -eq 1 && $helgrindout -eq 1 ]]; then
+   if [[ $valgridgout -ne 1 && $helgrindout -ne 1 ]]; then
     Compliation="PASS"
     Memory_leaks="FAIL"
     Thread_race="FAIL"
