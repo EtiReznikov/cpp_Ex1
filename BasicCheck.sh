14bc33b1faa9cf84a26c6a824643f50e89ab9f26
EtiReznikov <44700034+EtiReznikov@users.noreply.github.com>
2019-03-11 20:43:47 +0200

    Add files via upload

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -25,28 +25,28 @@ valgrind --leak-check=full --error-exitcode=1  ./$executable "${@:2}" > /dev/nul
 valgrind --tool=helgrind --error-exitcode=1 ./$executable "${@:2}"> /dev/null 2&>1
   helgrindout=$?
 
-   if [[ $valgridgout -eq 1 && $helgrindout -eq 1 ]]; then
+   if [[ $valgridgout -ne 1 && $helgrindout -ne 1 ]]; then
     Compliation="PASS"
     Memory_leaks="PASS"
     Thread_race="PASS"
     ans=0
    fi
 
-   if [[ $valgridgout -ne 1 && $helgrindout -eq 1 ]]; then
+   if [[ $valgridgout -eq 1 && $helgrindout -ne 1 ]]; then
     Compliation="PASS"
     Memory_leaks="FAIL"
     Thread_race="PASS"
     ans=2
    fi
 
-   if [[ $valgridgout -eq 1 && $helgrindout -ne 1 ]]; then
+   if [[ $valgridgout -ne 1 && $helgrindout -eq 1 ]]; then
     Compliation="PASS"
     Memory_leaks="PASS"
     Thread_race="FAIL"
     ans=1
    fi
 
-   if [[ $valgridgout -ne 1 && $helgrindout -ne 1 ]]; then
+   if [[ $valgridgout -eq 1 && $helgrindout -eq 1 ]]; then
     Compliation="PASS"
     Memory_leaks="FAIL"
     Thread_race="FAIL"
