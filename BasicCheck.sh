9ad9dd0c4bc20147a8a381c0b2241de684b90ab4
EtiReznikov <44700034+EtiReznikov@users.noreply.github.com>
2019-03-12 09:02:13 +0200

    Add files via upload

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -13,11 +13,16 @@ Compliation="FAIL"
 Memory_leaks="FAIL"
 Thread_race="FAIL"
 
+if [ -f Makefile ]; then
+
+
+
+
 cd $folderName
-make > /dev/null 2&>1
+make 
 secssesfullmake=$?
 
-if [[ $secssesfullMake -ne 0 ]]; then
+if [[ $secssesfullMake -eq 0 ]]; then
 valgrind --leak-check=full --error-exitcode=1  ./$executable "${@:2}" > /dev/null 2&>1
   valgridgout=$?
 valgrind --tool=helgrind --error-exitcode=1 ./$executable "${@:2}"> /dev/null 2&>1
@@ -28,31 +33,32 @@ valgrind --tool=helgrind --error-exitcode=1 ./$executable "${@:2}"> /dev/null 2&
     Memory_leaks="PASS"
     Thread_race="PASS"
     ans=0
-   fi
+   
 
-   if [[ $valgridgout -eq 1 && $helgrindout -ne 1 ]]; then
+   elif [[ $valgridgout -eq 1 && $helgrindout -ne 1 ]]; then
     Compliation="PASS"
     Memory_leaks="FAIL"
     Thread_race="PASS"
     ans=2
-   fi
+   
 
-   if [[ $valgridgout -ne 1 && $helgrindout -eq 1 ]]; then
+   elif [[ $valgridgout -ne 1 && $helgrindout -eq 1 ]]; then
     Compliation="PASS"
     Memory_leaks="PASS"
     Thread_race="FAIL"
     ans=1
-   fi
+   
 
-   if [[ $valgridgout -eq 1 && $helgrindout -eq 1 ]]; then
+   elif [[ $valgridgout -eq 1 && $helgrindout -eq 1 ]]; then
     Compliation="PASS"
     Memory_leaks="FAIL"
     Thread_race="FAIL"
     ans=3
-   fi
+   
+  fi
 
 fi
-
+fi
 cd $currentfolder
 echo "Compilation| Memory leaks| thread race" 
 echo "    "$Compliation"   |     "$Memory_leaks"    |     "$Thread_race"   " 
